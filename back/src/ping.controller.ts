0734d6d5e494f1cf880b19b2387d2ad7b0c0167b
Natalia-Barragan <natalia.barragannb@gmail.com>
2026-03-18 11:34:36 -0300

    cambio ping para render y bd

--- back/src/ping.controller.ts
+++ back/src/ping.controller.ts
@@ -1,13 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
+import { DataSource } from 'typeorm'; // Importamos el DataSource para la DB
 
 @Controller('ping')
 export class PingController {
+    constructor(private readonly dataSource: DataSource) { }
+
     @Get()
-    getPing() {
-        return {
-            status: 'ok',
-            message: 'Servidor activo',
-            timestamp: new Date().toISOString()
-        };
+    async getPing() {
+        try {
+            await this.dataSource.query('SELECT 1');
+
+            return {
+                status: 'ok',
+                database: 'connected',
+                timestamp: new Date().toISOString()
+            };
+        } catch (error) {
+            return {
+                status: 'error',
+                message: 'Server is awake but Database is not responding',
+                error: error.message
+            };
+        }
     }
 }
